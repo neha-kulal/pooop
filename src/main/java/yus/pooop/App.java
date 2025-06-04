c50dd65b8cbf6a0863b91e0cb6dc230fb677c891
neha-kulal <nehavijayakulal2004@gmail.com>
2025-06-04 15:11:52 +0530

    first commit

---
+++ src/main/java/yus/pooop/App.java
@@ -0,0 +1,13 @@
+package yus.pooop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
